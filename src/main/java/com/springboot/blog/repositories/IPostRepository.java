be1aab56cf4d23d74023b6777f56424b92ff76a5
Anh Nguyen <nganhh014@gmail.com>
2023-10-31 09:01:22 +0200

    create post repository

---
+++ src/main/java/com/springboot/blog/repositories/IPostRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.blog.repositories;
+
+import com.springboot.blog.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPostRepository extends JpaRepository<Post, Long> {
+}
